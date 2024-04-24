9a1a134b33c36cadf74617fbebe178825fa7b078
hoangnhandev <hnhan793@gmail.com>
2024-04-24 11:11:22 +0700

    Add Jost font family

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': 'Jost, Helvetica, Arial, sans-serif',
+    },
+    extend: {
+      spacing: {
+        's-1': '5px',
+        's-2': '10px',
+        's-3': '15px',
+        's-4': '20px',
+        's-5': '25px',
+        's-6': '30px',
+        's-7': '35px',
+        's-8': '40px',
+        's-9': '45px',
+        's-10': '50px',
+      },
+      colors: {
+        'heading': '#111111',
+        'primary': '#dd3327',
+        'btn-primary-bg': '#111111',
+        'submenu': '#555555',
+      },
+    },
   },
   plugins: [],
 }
